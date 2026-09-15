1e65f16734b4b692e22fdc243af2efd502b56de6
Matheus Moreira <matheusipsnobre@gmail.com>
2026-09-15 18:21:53 -0300

    trocando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <p>Ola</p>;
+};
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <img src="./logo.png" alt="" className="logo-img" />
+      <p className="btn-premium">Premium</p>
+    </div>
+  );
+};
+
+export default Header;
