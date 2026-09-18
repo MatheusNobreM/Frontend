02239622831effd33467aa0f44ad68bd94567dda
Matheus Moreira <matheusipsnobre@gmail.com>
2026-09-18 11:50:56 -0300

    feat: filter

---
+++ src/components/Filter.tsx
@@ -0,0 +1,26 @@
+import { Search } from "lucide-react";
+
+const Filter = () => {
+  return (
+    <div className="flex flex-col items-center mt-6">
+      <p className="font-bold text-xl  text-[#00A2F7]">
+        Busque o conteúdo que deseja assistir hoje
+      </p>
+      <p className="text-xs text-[#00A2F7]">Conteúdo exclusivo todos os dias</p>
+
+      <div className="flex items-center mt-2">
+        <input
+          placeholder="Curso de react"
+          type="text"
+          className="bg-white border border-[#F2F2F2] rounded-md w-[615px] h-[35px]"
+        ></input>
+        <div className="h-[35px] w-[35px] flex justify-center items-center">
+          <Search size={18} className="text-[#00A2F7]" />
+        </div>
+        <></>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Filter from "./components/Filter";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="flex flex-col">
+    <div className="flex flex-col h-screen bg-[#f4f8ff]">
       <Header />
-      <div className="content"> </div>
+      <div className="flex-1 max-w-[1200px] m-auto w-full">
+        <Filter />
+      </div>
       <Footer />
     </div>
   </StrictMode>,
